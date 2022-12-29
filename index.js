08d0738fbaa8634d00e79b891aa02784f7bbac1b
Digambaran <arjunsreekumar.1@gmail.com>
2022-12-29 19:35:50 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const sample_shield_signup = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello sample_shield_signup`}))
+  res.end()
+  
+}
+
+export default sample_shield_signup
